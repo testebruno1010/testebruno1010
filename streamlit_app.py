b22a877871265e596a4da90ce9a6e35cb0d10209
Testebruno1010 <114482492+testebruno1010@users.noreply.github.com>
2022-09-27 11:35:24 +0100

    Update streamlit_app.py
    fix bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Bluebarry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free_Range Egg')
-streamlit.text('🥑🍞 Avocado Toast)
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
