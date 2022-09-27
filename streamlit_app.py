832591895e2eb225dcf185d8c31a3a454dc220da
Testebruno1010 <114482492+testebruno1010@users.noreply.github.com>
2022-09-27 11:32:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New Healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluebarry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free_Range Egg')
